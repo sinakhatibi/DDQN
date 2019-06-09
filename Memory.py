7c9b2e3f709c83ea27c9409db9f8f0ccebfc4b25
Sina Khatibi <mr.sina.kh@gmail.com>
2019-06-09 12:59:39 +0200

    Add files via upload

---
+++ Memory.py
@@ -0,0 +1,16 @@
+import numpy as np
+from collections import deque
+
+
+class Memory:
+    def __init__(self, max_size=1000):
+        self.buffer = deque(maxlen=max_size)
+
+    def add(self, experience):
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        idx = np.random.choice(np.arange(len(self.buffer)),
+                               size=batch_size,
+                               replace=False)
+        return [self.buffer[ii] for ii in idx]
